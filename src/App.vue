b4ae8e6966033c9ea8ff9dbfdc4f2448c7811aa5
omur8819 <omur8819@gmail.com>
2021-06-27 22:19:15 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="card-wrapper">
+      <app-card @data="childData = $event"></app-card>
+      <app-card :name="title"></app-card>
+      <app-card :name="title"></app-card>
+    </div>
+    <div>
+      <button @click="changeName">Adımı değiştir</button>
+      <p>Child component üzerinden gelen veri {{childData}}</p>
+    </div>
+    <div class="form-wrapper">
+      <app-form></app-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card.vue';
+  import Form from './components/Form.vue';
+  export default {
+    components : {
+      appCard: Card,
+      appForm: Form,
+    },
+    data(){
+      return{
+        title: "omur",
+        childData: ""
+      }
+    },
+    methods: {
+      changeName(){
+        this.title = "bozdag"
+      }
+    }
+  };
+</script>
+
+<style>
+  .card-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 10rem;
+  }
+  .form-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 10rem;
+  }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img
+            class="card-img-top"
+            src="https://i4.hurimg.com/i/hurriyet/75/0x0/60c862244e3fe119909a689a.jpg"
+            alt="Card image cap"
+            />
+            <div class="card-body">
+            <h5 class="card-title">Galatasaray</h5>
+            <p class="card-text">
+                "To play in a collective way like the English, to signify a certain color and name and defeat non-Turkish teams."
+                Ali Sami Yen
+            </p>
+            <a href="https://www.galatasaray.org/anasayfa" class="btn btn-primary">Go to GS Website</a>
+            <p>Kullanıcı Adı : {{ name }}</p>
+            <button @click="sendToParent">Veriyi Parent'a gönder</button>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        // props: ["name",],
+        // props: {
+        //     name : [String, Array]
+        // },
+        props: {
+            name : {
+                type : String,
+                required: true,
+                default: "omurbozdag",
+            }
+        },
+        data(){
+            return{
+                childData: "",
+            }
+        },
+        methods: {
+            sendToParent(){
+                this.$emit("data", "kablosuzkedi");
+            }
+        }
+    }
+</script>
